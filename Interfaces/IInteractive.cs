11be69ffc0e8bf3ac05cbfb143046d0acaebcf1e
AlexHasntProblems <juniormistik4@gmail.com>
2025-02-19 14:24:47 +0300

    Delete Interfaces directory

--- Interfaces/IInteractive.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-
-public  interface IInteractive
-{
-    public IEnumerator Interact();
-}
--- Interfaces/IMovable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IMoveable
-{
-    public void Move();
-    public void Jump();
-}
